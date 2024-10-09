01f0777b4923488937ae9fd5e47b25cb0a113d88
Unrealrojo234 <otienoryan812@gmail.com>
2024-10-09 18:09:49 +0300

    First time commit

---
+++ app/src/main/java/com/example/finallydoneit/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.finallydoneit;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    public void handleGreet(View view) {
+        EditText name = findViewById(R.id.editTextText3);
+        Editable gotName =  name.getText();
+
+        TextView owner = findViewById(R.id.textView4);
+        owner.setText("Hey "+gotName+", nice to meet you, my name is $Cryptic$, the creator of this app");
+
+        name.setVisibility(View.INVISIBLE);
+    }
+}
